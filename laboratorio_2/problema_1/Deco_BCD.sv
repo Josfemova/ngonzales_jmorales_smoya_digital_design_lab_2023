8bd668679fd3c9124f59938b1ddcfb7da1a7a4e1
Sebas Moya <sebas0163@estudiantec.cr>
2023-02-28 20:41:18 -0600

    [Add] bcd deco

--- laboratorio_2/problema_1/Deco_BCD.sv
+++ laboratorio_2/problema_1/Deco_BCD.sv
@@ -1,9 +1,13 @@
 module top(
 input logic [3:0] bin,
-output logic [7:0] bcd
+output logic [13:0] catodo
 );
+logic [7:0] bcd;
+
 
 Bin2Bcd deco(bin,bcd);
+
+catodo_control control(bcd[3:0],bcd[7:4],catodo);
 
 
 endmodule
