18df70be704fea3c4f979e3d119b247b022db21c
Natalia Gonzalez <natigb@estudiantec.cr>
2023-03-04 21:39:02 -0600

    l2p3: testbench

--- laboratorio_2/problema_3/contador_tb.sv
+++ laboratorio_2/problema_3/contador_tb.sv
@@ -1,8 +1,10 @@
 module contador_tb();
-	logic clk, reset;
+	logic clk, reset ;
 	
-	logic [5:0] num;
-	logic [13:0] cat;
+	logic [1:0] num_2bit;
+	logic [3:0] num_4bit;
+	logic [5:0] num_6bit;
+	logic [13:0] display_6bit;
 	
 	
 	
@@ -11,27 +13,51 @@ module contador_tb();
 	
 	contador #(6) count_6(.clk(clk),
 						  .reset(reset),
-						  .number(num),
-						  .catodo(cat));
-	
-	initial begin 
-		
-		clk=0;
-		forever #5 clk=~clk;
-	end
-	
+						  .number(num_6bit),
+						  .catodo(display_6bit));
+	contador #(4) count_4(.clk(clk),
+						  .reset(reset),
+						  .number(num_4bit));
+	contador #(2) count_2(.clk(clk),
+						  .reset(reset),
+						  .number(num_2bit));
+						  
+
 	initial begin
 		
 		reset=0;
-		#20;
-		reset=1;
-		#100
-		reset = 0;
+		#10
+		assert(num_2bit == 3) else
+		$error("2bit reset failed");
 		
-		#100
+		assert(num_4bit == 15)else
+		$error("4bit reset failed");
+		
+		assert(num_6bit == 63)else
+		$error("6bit reset failed");
 		reset = 1;
-		#100
-		reset = 0;
+
+	end
+	
+	initial begin 
+		clk = 0;
+		forever #5 clk=~clk;
+		
+	end
+	
+	always @ (negedge clk) begin
+		if(num_2bit == 0) begin
+			$display("2bit finished counting to ", num_2bit);
+		end
+		
+		if(num_4bit == 0) begin
+			$display("4bit finished counting to ", num_4bit);
+		end
+	
+		if(num_6bit == 0) begin
+			$display("6bit finished counting to ", num_6bit);
+			$finish;
+		end
 		
 	end
 	
