98207d1ac01229b958a06480cd73b6f9ec12b6e5
josfemova <josfemova@gmail.com>
2023-03-19 18:58:42 -0600

    l3_adder: mover archivos de sumador al folder base del laboratorio

--- laboratorio_3/sumador.sv
+++ laboratorio_3/sumador.sv
@@ -1,24 +1,25 @@
-module sumador #(parameter N = 32)(input logic [N:0] num1,
-													  input logic [N:0] num2,
-													  output logic [N:0] result,
-													  output logic c_o);
-	
+module sumador #(parameter WIDTH = 32)(
+	input logic [WIDTH-1:0] num1,
+	input logic [WIDTH-1:0] num2,
+	output logic [WIDTH-1:0] result,
+	output logic c_o
+);	
 	logic carry_out;
 	
-	logic [N:0] carry_reg;
+	logic [WIDTH-1:0] carry_reg;
 	
 	genvar i;
 	generate
-	for (i=0; i<N; i=i+1)
+	for (i=0; i<WIDTH; i=i+1)
 		begin: generate_adder
 			if (i==0)
 				half_adder f(num1[0], num2[0], result[0], carry_reg[0]);
 			else
 				full_adder f(num1[i], num2[i], carry_reg[i-1], result[i], carry_reg[i]);
 		end
-	assign carry_out = carry_reg[N-1];
+	assign carry_out = carry_reg[WIDTH-1];
 	endgenerate
-	assign c_o = carry_reg[N-1];
+	assign c_o = carry_reg[WIDTH-1];
 													  
 endmodule
 
