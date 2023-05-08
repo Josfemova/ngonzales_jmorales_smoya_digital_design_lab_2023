b1ad5b160bb0ad21ad964ecfd53737bc248cc848
Sebas Moya <sebas0163@estudiantec.cr>
2023-05-07 22:05:18 -0600

    [Update] testbench

--- laboratorio_4/neo_impl/juego_tb.sv
+++ laboratorio_4/neo_impl/juego_tb.sv
@@ -3,7 +3,7 @@ logic [11:0] score;
 logic [1:0] draw_state;
 logic clk = 0;
 logic reset = 1;
-logic meta = 11;
+logic [3:0]meta = 3;
 logic [3:0] dir = 4'b0;
 logic [3:0] gmatrix[0:3][0:3] = '{default:0}; // valor por casilla es potencia de 2
 
@@ -22,9 +22,21 @@ juego_neo jg(
 initial begin
 	#4
 	reset = 0;
-	dir[3] = 1;
+	dir = 4'b0001;//mov a la izquierda
 	#10
-	dir[3] = 0;
+	dir = 4'b0010; //Mov a arriba
+	#10
+	dir = 4'b1000; //Mov a la derecha
+	#10
+	dir = 4'b0100; // Mov a abajo
+	#10
+	dir = 4'b0000; //No hay movimiento
+	#10
+	dir = 4'b0100; //Mov a abajo
+	#10
+	dir = 4'b0100; // mov a abajo
+	#10
+	reset =1;    // reset del juego 
 end
 
 endmodule
